d014ebe5b18c7b498516a0ad6a74d48450282ca0
celalcolak94 <celalcolak94@gmail.com>
2023-07-13 20:16:53 +0300

    CartSummary

---
+++ E-Ticaret/ViewComponents/CartSummary.cs
@@ -0,0 +1,23 @@
+﻿using E_Ticaret.Data;
+using E_Ticaret.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Ticaret.ViewComponents
+{
+    public class CartSummary : ViewComponent
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public CartSummary(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var cart = _httpContextAccessor?.HttpContext?.Session.GetCart("Cart");
+
+            return View(cart);
+        }
+    }
+}
